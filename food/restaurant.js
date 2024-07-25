b0e903251fe2d1d87ba5708b4f8b2a4538d9b8fb
Alan Bui <alanb3350@gmail.com>
2024-07-25 16:21:45 -0400

    Added alapar

--- food/restaurant.js
+++ food/restaurant.js
@@ -48,7 +48,7 @@ if (searchParams.has('id')){
             //PHOTOS LINK
             const photoLink = document.getElementsByClassName('link-photo')[0];
             photoLink.style.backgroundImage = `url("../assets/images/food/`+id+`.svg")`;
-            photoLink.href = data[id]["photoLink"] === "" ? "./photos.html?id="+id : data[id]["photoLink"];
+            photoLink.href = ((data[id]["photoLink"] === "") ? ("./photos.html?id="+id) : data[id]["photoLink"]);
 
             //REVIEW
             setElementById('review', review);

9c63aeaa92f27984ee5903717fcb4d6672b3b832
Alan Bui <alanb3350@gmail.com>
2024-07-28 11:11:43 -0400

    added filter

--- food/filter.js
+++ food/filter.js
@@ -1,14 +1,28 @@
 const filter = (filt) => {
+    var buttonElement = document.getElementById(filt);
+    if (buttonElement.className === 'btn clicked'){
+        buttonElement.className = 'btn';
+    }
+    else buttonElement.className = 'btn clicked';
+
+    const filters = [];
+    var btns = document.getElementsByClassName('btn');
+    for (let i=0; i<btns.length; i++){
+        if (btns[i].className === 'btn clicked'){
+            filters.push(btns[i].id);
+        }
+    }
+
     var cards = document.getElementsByClassName('card');
 
     for (let i=0; i<cards.length; i++){
         var card = cards[i];
-        
-        if (!(find(filt, card.classList))){
-            card.style.display = 'none';
+
+        if (match(filters, card.classList)){
+            card.style.display = '';
         }
         else{
-            card.style.display = '';
+            card.style.display = 'none';
         }
     }
 }
@@ -18,4 +32,11 @@ const find = (want, list) => {
         if (want === list[i]) return true;
     }
     return false;
+}
+
+const match = (wantList, list) => {
+    for (let i=0; i<wantList.length; i++){
+        if (!(find(wantList[i], list))) return false;
+    }
+    return true;
 }
